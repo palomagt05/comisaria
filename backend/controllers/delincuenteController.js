7c38a0076863451fe01432e9639ffb4f0e0f4e4b
palomagt05 <gatitogt919@gmail.com>
2024-07-17 16:48:17 -0600

    Controller- Contralador de delincuente modificado

--- backend/controllers/delincuenteController.js
+++ backend/controllers/delincuenteController.js
@@ -3,30 +3,30 @@ const bcrypt = require('bcrypt');
 
 const addDelincuente = async (req, res) => {
     try {
-        const { curp,name, tel,direccion} = req.body;
+        const { curp, name, tel, direccion } = req.body;
 
-        // Verificar si el nombre de usuario ya existe
+        // Verificar si el CURP ya existe
         const checkUserQuery = 'SELECT * FROM delincuentes WHERE CURP = ?';
         const [existingUser] = await connection.execute(checkUserQuery, [curp]);
 
         if (existingUser.length > 0) {
-            return res.status(400).json({ message: 'El Curp ya esta registrado' });
+            return res.status(400).json({ message: 'El CURP ya está registrado' });
         }
 
-        // Encriptar la contraseña usando bcrypt
+        // Encriptar la CURP y dirección usando bcrypt
         const saltRounds = 10;
-        const hashedcurp = await bcrypt.hash(curp, saltRounds);
-        const hashedireccion = await bcrypt.hash(direccion, saltRounds);
+        const hashedCurp = await bcrypt.hash(curp, saltRounds);
+        const hashedDireccion = await bcrypt.hash(direccion, saltRounds);
 
-        // Ejecutar la consulta para insertar un nuevo usuario
-        const query = `INSERT INTO delincuentes (CURP, Nombre, Telefono, Direccion) VALUES (?, ?, ?, ?)`;
-        const values = [hashedcurp,tel,name,hashedireccion];
+        // Ejecutar la consulta para insertar un nuevo delincuente
+        const query = 'INSERT INTO delincuentes (CURP, Nombre, Telefono, Direccion) VALUES (?, ?, ?, ?)';
+        const values = [hashedCurp, name, tel, hashedDireccion];
 
         const [result] = await connection.execute(query, values);
 
-        res.status(201).json({ message: 'Delincuente agregado con exito', userId: result.insertId });
+        res.status(201).json({ message: 'Delincuente agregado con éxito', userId: result.insertId });
     } catch (error) {
-        res.status(500).json({ message: 'Error al agregar usuario', error });
+        res.status(500).json({ message: 'Error al agregar delincuente', error });
     }
 };
 
