d4f8dc01dcfac3dbb7948fbb0716dcb6e65498e1
palomagt05 <gatitogt919@gmail.com>
2024-07-17 16:47:42 -0600

    Agregación de la ruta agregar delincuentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginView from './view/LoginView';
-import LoginViewModel from './viewmodel/LoginViewModel';
-import AdminView from './view/AdminView';  // Asegúrate de crear estos componentes
+import AdminView from './view/AdminView';
 import PoliceView from './view/PoliceView';
 import ProtectedRoute from './components/ProtectedRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddUserView from './view/AddUserView';
 import AddPoliView from './view/AddPoliView';
 import AddArmaView from './view/AddArmaView';
-
+import AddDelincuenteView from './view/AddDelincuenteView';
 
 const App = () => {
     return (
@@ -18,9 +17,10 @@ const App = () => {
                 <Route path="/" element={<LoginView />} />
                 <Route path="/admin" element={<ProtectedRoute element={<AdminView />} />} />
                 <Route path="/police" element={<ProtectedRoute element={<PoliceView />} />} />
-                <Route path="/admin/add-user" element={<ProtectedRoute element={<AddUserView />}/>} />
-                <Route path="/admin/add-poli" element={<ProtectedRoute element={<AddPoliView />}/>} />
-                <Route path="/admin/add-arma" element={<ProtectedRoute element={<AddArmaView />}/>} />
+                <Route path="/admin/add-user" element={<ProtectedRoute element={<AddUserView />} />} />
+                <Route path="/admin/add-poli" element={<ProtectedRoute element={<AddPoliView />} />} />
+                <Route path="/admin/add-arma" element={<ProtectedRoute element={<AddArmaView />} />} />
+                <Route path="/admin/add-delincuente" element={<ProtectedRoute element={<AddDelincuenteView />} />} />
             </Routes>
         </Router>
     );
