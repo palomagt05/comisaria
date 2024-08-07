06fd8e7e53c1d5bb9656b602f07cb697d53c763b
palomagt05 <gatitogt919@gmail.com>
2024-08-06 19:00:41 -0600

    Backend-Controller-CasoController- cambio de encriptacion a cryto para poder recuperar datos depués

--- backend/controllers/casoController.js
+++ backend/controllers/casoController.js
@@ -1,7 +1,15 @@
-// controllers/casoController.js
+const crypto = require('crypto');
+const connection = require('../database');
 
-const pool = require('../database');
-const bcrypt = require('bcrypt');
+// Función para desencriptar datos
+const decrypt = (encryptedData, iv, key) => {
+    const algorithm = 'aes-256-cbc';
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+
+    return decrypted;
+};
 
 const registerCase = async (req, res) => {
     const { Codigo_Caso, CURP_Delincuente, Juzgado } = req.body;
@@ -12,16 +20,16 @@ const registerCase = async (req, res) => {
 
     try {
         // Buscar el CURP encriptado de los delincuentes en la base de datos
-        const findDelincuenteQuery =  'SELECT CURP FROM delincuentes';
-        const [delincuentes] = await pool.query(findDelincuenteQuery);
+        const findDelincuenteQuery = 'SELECT CURP, iv_curp, llave_curp FROM delincuentes';
+        const [delincuentes] = await connection.execute(findDelincuenteQuery);
 
-        // Verificar si algún CURP encriptado coincide con el CURP proporcionado
+        // Verificar si algún CURP desencriptado coincide con el CURP proporcionado
         let delincuenteFound = false;
         let delincuenteCURP;
 
         for (const delincuente of delincuentes) {
-            const match = await bcrypt.compare(CURP_Delincuente, delincuente.CURP);
-            if (match) {
+            const decryptedCURP = decrypt(delincuente.CURP, delincuente.iv_curp, delincuente.llave_curp);
+            if (decryptedCURP === CURP_Delincuente) {
                 delincuenteFound = true;
                 delincuenteCURP = delincuente.CURP;
                 break;
@@ -33,7 +41,7 @@ const registerCase = async (req, res) => {
         }
 
         // Registrar el caso en la base de datos usando el CURP encriptado
-        const result = await pool.query('INSERT INTO caso_delincuente (Codigo_Caso, CURP_Delincuente, Juzgado) VALUES (?, ?, ?)', [Codigo_Caso, delincuenteCURP, Juzgado]);
+        const result = await connection.execute('INSERT INTO caso_delincuente (Codigo_Caso, CURP_Delincuente, Juzgado) VALUES (?, ?, ?)', [Codigo_Caso, delincuenteCURP, Juzgado]);
         return res.status(201).json({ message: 'Caso registrado exitosamente', result });
     } catch (error) {
         console.error(error);
